eaa36be92679d73e11f94bd1ec9d5a1148d51c4c
Francisco Caro Diaz <francisco.caro@s-dos.es>
2018-05-22 20:40:47 +0200

    First commit with init setup project.

---
+++ app/src/main/java/fcd/com/festival2018/MainActivity.kt
@@ -0,0 +1,12 @@
+package fcd.com.festival2018
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
