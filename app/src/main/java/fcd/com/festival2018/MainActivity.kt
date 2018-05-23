29b96ba856e1d59673bd6e563423b6857a1b21ba
Francisco Caro Diaz <francisco.caro@s-dos.es>
2018-05-23 17:41:23 +0200

    Kotlin android extensions.

--- app/src/main/java/fcd/com/festival2018/MainActivity.kt
+++ app/src/main/java/fcd/com/festival2018/MainActivity.kt
@@ -2,11 +2,14 @@ package fcd.com.festival2018
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigationView.selectedItemId = R.id.action_menu_middle;
     }
 }
