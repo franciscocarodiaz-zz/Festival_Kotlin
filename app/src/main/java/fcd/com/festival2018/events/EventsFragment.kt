b125be17fd9d554749499343c59ea19179763708
Francisco Caro Diaz <francisco.caro@s-dos.es>
2018-05-23 17:00:33 +0200

    Clase commons. Clases abstractas.

--- app/src/main/java/fcd/com/festival2018/events/EventsFragment.kt
+++ app/src/main/java/fcd/com/festival2018/events/EventsFragment.kt
@@ -1,20 +1,14 @@
 package fcd.com.festival2018.events
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.arequa.commons.BaseFragment
 import fcd.com.festival2018.R
 
 /**
  * Created by FCD on 22/05/2018.
  */
-class EventsFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragments_events, container, false)
-        return view
-
+class EventsFragment : BaseFragment() {
+    
+    override fun getLayoutResId(): Int {
+        return R.layout.fragments_events;
     }
 }
---
+++ commons/src/main/java/com/arequa/commons/BaseFragment.kt
@@ -0,0 +1,21 @@
+package com.arequa.commons
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by FCD on 23/05/2018.
+ */
+abstract class BaseFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(getLayoutResId(), container, false)
+        return view
+    }
+
+    abstract fun getLayoutResId() : Int
+
+}
