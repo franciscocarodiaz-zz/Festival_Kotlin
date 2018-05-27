23019f4e87f4f49190075c619b29e479703efa4e
Francisco Caro Diaz <franciscocarodiaz@gmail.com>
2018-05-27 20:59:19 +0200

    Custom binding

--- commons/src/main/java/com/arequa/commons/DataBindingRecyclerAdapter.kt
+++ commons/src/main/java/com/arequa/commons/DataBindingRecyclerAdapter.kt
@@ -1,13 +1,16 @@
 package com.arequa.commons
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 
 /**
  * Created by FCD on 27/05/2018.
  */
-class DataBindingRecyclerAdapter<MODEL>(val itemLayoutResId : Int) : RecyclerView
-.Adapter<DataBindingViewHolder<MODEL>>() {
+class DataBindingRecyclerAdapter<MODEL>(val itemVariableId: Int, val itemLayoutResId: Int) :
+        RecyclerView.Adapter<DataBindingViewHolder<MODEL>>() {
 
     val items = mutableListOf<MODEL>()
 
@@ -16,8 +19,12 @@ class DataBindingRecyclerAdapter<MODEL>(val itemLayoutResId : Int) : RecyclerVie
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
-        val view = parent.inflate(itemLayoutResId)
-        return DataBindingViewHolder(view)
+        val binding: ViewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
+                itemLayoutResId,
+                parent,
+                false)
+
+        return DataBindingViewHolder(itemVariableId, binding)
     }
 
     override fun onBindViewHolder(holder: DataBindingViewHolder<MODEL>, position: Int) {
--- commons/src/main/java/com/arequa/commons/DataBindingViewHolder.kt
+++ commons/src/main/java/com/arequa/commons/DataBindingViewHolder.kt
@@ -1,13 +1,15 @@
 package com.arequa.commons
 
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
-import android.view.View
 
 /**
  * Created by FCD on 27/05/2018.
  */
-class DataBindingViewHolder<MODEL>(view: View) : RecyclerView.ViewHolder(view) {
+class DataBindingViewHolder<MODEL>(val itemVariableId: Int, val binding: ViewDataBinding) :
+        RecyclerView.ViewHolder(binding.root) {
     fun bindItem(item: MODEL) {
-
+        binding.setVariable(itemVariableId, item)
+        binding.executePendingBindings()
     }
 }
