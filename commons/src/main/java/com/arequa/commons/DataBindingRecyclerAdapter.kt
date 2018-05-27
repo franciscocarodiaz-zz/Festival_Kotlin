0d4c9b44d95b103982fd0e20ce6c75a5a456b570
Francisco Caro Diaz <franciscocarodiaz@gmail.com>
2018-05-27 20:47:11 +0200

    DataBindingRecyclerAdapter custom.

---
+++ commons/src/main/java/com/arequa/commons/DataBindingRecyclerAdapter.kt
@@ -0,0 +1,28 @@
+package com.arequa.commons
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+/**
+ * Created by FCD on 27/05/2018.
+ */
+class DataBindingRecyclerAdapter<MODEL>(val itemLayoutResId : Int) : RecyclerView
+.Adapter<DataBindingViewHolder<MODEL>>() {
+
+    val items = mutableListOf<MODEL>()
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<MODEL> {
+        val view = parent.inflate(itemLayoutResId)
+        return DataBindingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder<MODEL>, position: Int) {
+        val item = items[position]
+        holder.bindItem(item)
+    }
+
+}
---
+++ commons/src/main/java/com/arequa/commons/DataBindingViewHolder.kt
@@ -0,0 +1,13 @@
+package com.arequa.commons
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by FCD on 27/05/2018.
+ */
+class DataBindingViewHolder<MODEL>(view: View) : RecyclerView.ViewHolder(view) {
+    fun bindItem(item: MODEL) {
+
+    }
+}
