093b769a2fd2f691fbe6298b293ddce4c50ed5e1
Francisco Caro Díaz <franciscocarodiaz@gmail.com>
2018-06-09 13:25:34 +0200

    DataBinding fixed.

--- commons/src/main/java/com/arequa/commons/BindingAdapters.kt
+++ commons/src/main/java/com/arequa/commons/BindingAdapters.kt
@@ -8,7 +8,7 @@ import com.bumptech.glide.Glide
  * Created by FCD on 28/05/2018.
  */
 
-@BindingAdapter("imageUrl")
+@BindingAdapter("imgUrl")
 fun setImageUrl(img: ImageView, url: String) {
     Glide.with(img).load(url).into(img)
 }
